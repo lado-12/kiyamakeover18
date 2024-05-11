78e8ec444616777a059cdc73a6a25094b0c5ce8f
Khushi Kumari <132393604+lado-12@users.noreply.github.com>
2024-05-11 15:19:36 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
   return (
     <div className="flex justify-end items-center">
       <button className="block md:hidden" onClick={toggleMenu}>
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
         </svg>
       </button>
       {isOpen && (
-        <div className="fixed top-0 right-0 bottom-0 left-0 bg-gray-900 bg-opacity-75 z-50">
+        <div className="fixed top-10 right-10 bottom-10 left-10 bg-gray-900 bg-opacity-45 z-50">
           <div className="flex justify-end p-4">
             <button className="text-white" onClick={toggleMenu}>
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
