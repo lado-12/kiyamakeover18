27aa38a1cdcf7f7cc2f00724d098c4df5796e750
Khushi Kumari <132393604+lado-12@users.noreply.github.com>
2024-05-11 14:58:11 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,16 +10,7 @@ function Navbar() {
    <>
    
 
-   <nav className="fixed top-0 left-0 right-0 flex justify-center items-center mt-2 text-4xl">
-  <div className="font-serif">
-    <a href="" className="flex justify-center space-x-3 rtl:space-x-reverse">
-      <img src="https://img.icons8.com/external-photo3ideastudio-lineal-photo3ideastudio/64/external-makeup-supermarket-photo3ideastudio-lineal-photo3ideastudio.png" alt="makeup"/>
-      <span className="self-center text-yellow-800 font-semibold whitespace-nowrap">
-        <u>KiyaMakeover18</u>
-      </span>
-    </a>
-  </div>
-</nav>
+   
 
 <img className='text-center ml-20 mt-16 size-52 rounded-full ' src={a4} alt="external-makeup-hobbies-and-interest-smashingstocks-mixed-smashing-stocks"/>
    <div className="flex justify-end items-center">
