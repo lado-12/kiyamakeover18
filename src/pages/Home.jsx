16e5981f99c0142de16dce25b78da48b0178c324
Khushi Kumari <132393604+lado-12@users.noreply.github.com>
2024-05-11 14:58:39 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,6 +42,16 @@ function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
         <Navbar/>
+      <nav className="fixed top-0 left-0 right-0 flex justify-center items-center mt-2 text-4xl">
+  <div className="font-serif">
+    <a href="" className="flex justify-center space-x-3 rtl:space-x-reverse">
+      <img src="https://img.icons8.com/external-photo3ideastudio-lineal-photo3ideastudio/64/external-makeup-supermarket-photo3ideastudio-lineal-photo3ideastudio.png" alt="makeup"/>
+      <span className="self-center text-yellow-800 font-semibold whitespace-nowrap">
+        <u>KiyaMakeover18</u>
+      </span>
+    </a>
+  </div>
+</nav>
         
          <h1   className=' text-center  text-yellow-800 font-semibold font-serif text-2xl'>
             {headerText}
